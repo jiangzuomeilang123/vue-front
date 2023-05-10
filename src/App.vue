e8fb1a354777a823498b7ec393a46eba67d82f9e
meijh <meijh@fosun.com>
2023-05-11 03:36:45 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <ul>
-      <li v-for="item in list" :key="item.id">{{item.id}}:{{item.name}}abc</li>
+      <li v-for="item in list" :key="item.id">abc{item.id}}:{{item.name}}</li>
     </ul>
 
     <el-form
